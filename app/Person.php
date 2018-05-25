efa40c7fe79011e419795237e2e1fb01e44703c3
Martín Vásquez <pedromvasquezb@gmail.com>
2018-05-25 05:00:34 +0000

    Add relationships to Person and Pet models

--- app/Person.php
+++ app/Person.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Person extends Model
 {
-    //
+    public function pets()
+    {
+        return $this->hasMany('App\Pet');
+    }
+
+    public function user()
+    {
+        return $this->hasOne('App\User');
+    }
 }
--- app/Pet.php
+++ app/Pet.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pet extends Model
 {
-    //
+    public function owner()
+    {
+        return $this->belongsToOne('App\Person');
+    }
+
+    public function animalType()
+    {
+        return $this->hasOne('App\AnimalType');
+    }
 }
