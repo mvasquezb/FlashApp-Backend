b09b11474ae4cf82dfe4a5f6bd12702262e66ec5
Martín Vásquez <pedromvasquezb@gmail.com>
2018-05-25 14:27:07 +0000

    Modify S3Helper

--- app/Helpers/S3Helper.php
+++ app/Helpers/S3Helper.php
@@ -6,6 +6,9 @@ class S3Helper {
     // Hardcoded for now
     public static function getFileUrl($filePath) {
         $urlBase = 'https://s3.us-east-2.amazonaws.com/flashapp-bucket';
-        return "$urlBase$filePath";
+        if (starts_with($filePath, '/')) {
+            return "$urlBase$filePath";
+        }
+        return $filePath;
     }
 }
