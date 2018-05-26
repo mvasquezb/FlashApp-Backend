b473ad678e4604425d8dc46eb6c6d5b090f8234b
Martin Vasquez <pedromvasquezb@gmail.com>
2018-05-26 17:45:06 -0500

    Update User model

--- app/User.php
+++ app/User.php
@@ -7,6 +7,30 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    public function fullName()
+    {
+        return "$this->firstName $this->lastName";
+    }
 
     public function pets()
     {
@@ -22,24 +46,4 @@ class User extends Authenticatable
     {
         return $this->hasManyThrough('App\Service', 'App\ServiceCustomer');
     }
-
-    use Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
 }
