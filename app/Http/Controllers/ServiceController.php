2177b3c64ce399515a8d851102783b71aafaa7a1
Unknown <a.huamani@pucp.pe>
2018-06-23 16:56:27 -0500

    fix

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -89,7 +89,7 @@ class ServiceController extends Controller
     }
 
     public function getServicebyCategory($idCategory){
-        $serviceStatusid= ServiceStatus::where('name', 'active')->value('id');
+        $serviceStatusid= ServiceStatus::where('name', 'activo')->value('id');
         $services = Service::where('type_id', $idCategory)->where('status_id', $serviceStatusid)->get();
         return $services;
         // $services = ServiceType::where('')
